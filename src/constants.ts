0a6804bfb994a128e194245bc3624d110f37da6a
GergoN02 <nemethgergo02@gmail.com>
2022-04-01 18:51:47 +0100

    Setup Express middleware

---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const __prod__ = process.env.NODE_ENV === "production";
+export const COOKIE_NAME = "qid";
+export const ONE_DAY = 1000 * 60 * 60 * 24;
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import session from "express-session";
 import Redis from "ioredis";
+import cors from "cors";
+import "dotenv/config";
 import { createConnection } from "typeorm";
 import typeormConfig from "./typeorm-config";
+import { COOKIE_NAME, ONE_DAY, __prod__ } from "./constants";
 
 
 const main = async () => {
@@ -21,6 +24,36 @@ const main = async () => {
 
     // Redis client for interacting with the Redis Store
     const redis = new Redis();
+
+    // Allow request access from defined origin with enabled creditential auth
+    app.use(
+        cors({
+            origin: "http://localhost:3000",
+            credentials: true,
+        }));
+
+    // Setup session with cookie initialized in redis store, attach redis client to session, set cookie params
+    app.use(
+        session({
+            name: COOKIE_NAME,
+            store: new RedisStore({
+                client: redis,
+                disableTouch: true
+            }),
+            cookie: {
+                maxAge: ONE_DAY * 365 * 10, // 10 years 
+                httpOnly: true,
+                sameSite: "lax", //CSRF
+                secure: __prod__
+            },
+            saveUninitialized: false,
+            secret: process.env.COOKIE_SECRET as string,
+            resave: false
+        })
+    );
+
+
+
 
 
     // Listen to incoming HTTP requests on defined port
