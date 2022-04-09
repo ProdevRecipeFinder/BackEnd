343dea03febe79996475ee8dd609810be88ff960
GergoN02 <nemethgergo02@gmail.com>
2022-04-09 13:59:22 +0100

    fixed error message responses

--- src/utils/errorHandling/errorMsg.ts
+++ src/utils/errorHandling/errorMsg.ts
@@ -1,70 +1,70 @@
 
 export const EMAI_INVALID = [
     {
-        field: "Email",
+        field: "email",
         message: "A valid email is required"
     }
 ];
 
 export const EMAIL_TAKEN = [
     {
-        field: "Email",
+        field: "email",
         message: "Email already registered"
     }
 ];
 
 export const EMAIL_NOTFOUND = [
     {
-        field: "Email",
+        field: "email",
         message: "This Email does not exist"
     }
 ]
 
 export const UNAME_INVALID = [
     {
-        field: "Username",
+        field: "username",
         message: "Username needs to be 5 - 15 characters, and cannot contain special symbols"
     }
 ];
 
 export const UNAME_TAKEN = [
     {
-        field: "Username",
+        field: "username",
         message: "This Username is already registered"
     }
 ];
 
 export const UNAME_NOTFOUND = [
     {
-        field: "Username",
+        field: "username",
         message: "This Username does not exist"
     }
 ];
 
 export const PASS_INVALID = [
     {
-        field: "Password",
+        field: "password",
         message: "Password needs to be 6 - 16 characters"
     }
 ];
 
 export const PASS_INCORRECT = [
     {
-        field: "Password",
+        field: "password",
         message: "This Password is incorrect"
     }
 ]
 
 export const TOKEN_INVALID = [
     {
-        field: "Token",
+        field: "token",
         message: "Token invalid or expired"
     }
 ];
 
 export const TOKEN_ERR_GENERIC = [
     {
-        field: "Token",
+        field: "token",
         message: "There was an issue validating the session token"
     }
 ]
