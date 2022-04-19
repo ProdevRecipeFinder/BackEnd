94c6d749e2aafbc2788182ba04aaf9e9b2854825
GergoN02 <nemethgergo02@gmail.com>
2022-04-19 22:51:43 +0100

    added arrays to saveStatus Resolver

--- src/resolvers/UserSavedRecipeRes.ts
+++ src/resolvers/UserSavedRecipeRes.ts
@@ -5,22 +5,29 @@ import { ServerContext } from "../types";
 @Resolver(UserSavedRecipes)
 export class UserSavedRecipesResolver {
 
-    @Query(() => Boolean)
+    @Query(() => [Boolean])
     async getSavedStatus(
-        @Arg("recipe_id") recipe_id: number,
+        @Arg("recipe_ids", () => [Number]) recipe_ids: number[],
         @Ctx() { req }: ServerContext
     ) {
+
+        let responseArray: boolean[] = [];
         const userId: number = req.session.userId;
         if (!userId) {
             return false
         }
-        const found = await UserSavedRecipes.findOne({
-            user_id: userId,
-            recipe_id: recipe_id
-        });
-        if (!found) {
-            return false;
+
+        for (let i = 0; i < recipe_ids.length; i++) {
+            const found = await UserSavedRecipes.findOne({
+                user_id: userId,
+                recipe_id: recipe_ids[i]
+            });
+            if (!found) {
+                responseArray.push(false);
+            }
+            responseArray.push(true);
         }
-        return true;
+
+        return responseArray;
     }
 }
