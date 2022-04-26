4903b1c939105f533fbe1df4dac902110477577b
GergoN02 <nemethgergo02@gmail.com>
2022-04-26 17:12:45 +0100

    added recommended resolvers and authentication error now throws correct error

--- src/middleware/checkAuth.ts
+++ src/middleware/checkAuth.ts
@@ -2,9 +2,14 @@ import { MiddlewareFn } from "type-graphql";
 import { ServerContext } from "../types";
 
 export const checkAuth: MiddlewareFn<ServerContext> = ({ context }, next) => {
-    if (context.req.session.userId) {
-        throw new Error("not authenticated");
-    }
+  if (context.req.session.userId) {
+    throw new Error("not authenticated");
+  }
 
-    return next();
+  return next();
 };
+
+
+export const throwAuthError = () => {
+  throw new Error("not authenticated to execute this request");
+}
